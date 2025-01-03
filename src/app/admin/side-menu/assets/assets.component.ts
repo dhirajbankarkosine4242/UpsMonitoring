071d1ac09ff37e4d4b4e412119da84827d7f1339
dhirajbankarkosine4242 <dhirajbankarkosine@gmail.com>
2025-01-03 11:35:17 +0530

    sixth commit

--- src/app/admin/side-menu/assets/assets.component.ts
+++ src/app/admin/side-menu/assets/assets.component.ts
@@ -1,37 +1,74 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HttpService } from '../../../service/http.service';
+import { AnalyticsComponent } from "./analytics/analytics.component";
+import { HistoryComponent } from "./history/history.component";
+import { ViewComponent } from "./view/view.component";
 
 @Component({
   selector: 'app-assets',
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, HistoryComponent, AnalyticsComponent, ViewComponent],
   templateUrl: './assets.component.html',
   styleUrl: './assets.component.css'
 })
 export class AssetsComponent {
 
-  data: any;
-  selectedId:any
-  selectedDeviceDetails: any = null;
+  isExpanded = false;
+  currentTab = 'viewTab';
+  data:any;
+  selectedDeviceId:any;
+  // @ViewChild('viewTab') viewTab!: ViewComponent;
+  // @ViewChild('analyticsTab') analyticsTab!: AnalyticsComponent;
+  // @ViewChild('historyTab') historyTab!: HistoryComponent;
 
-  constructor(private service: HttpService) {}
+  constructor(private service:HttpService){};
 
-  ngOnInit() {
-    this.getAssetsList();
+  ngOnInit(): void {
+    this.getAssetsList()
   }
-
+  
   getAssetsList() {
     this.service.get('assets').subscribe((response: any[]) => {
       this.data = response;
     });
   }
 
-  onDevIdClick(id: string) {
-    this.service.get('asset', id).subscribe((response: any) => {
-      this.selectedDeviceDetails = response;
-      this.selectedId = response.devId;
-    });
+  expandRow(devId:any): void {
+    this.isExpanded = !this.isExpanded;
+    this.selectedDeviceId = devId;
   }
+
+  switchTab(tab: string): void {
+    this.currentTab = tab;
+    // this.invokeTabMethod(tab);
+  }
+
+  // invokeTabMethod(tab: string) {
+  //   switch (tab) {
+  //     case 'viewTab':
+  //       this.viewTab?.ngOnInit();
+  //       break;
+  //     case 'analyticsTab':
+  //       this.analyticsTab?.ngOnInit();
+  //       break;
+  //     // case 'historyTab':
+  //     //   this.historyTab?.ngOnChanges();
+  //     //   break;
+  //     case 'historyTab':
+  //       if (this.historyTab) {
+  //         const changes = {
+  //           deviceId: {
+  //             previousValue: null,
+  //             currentValue: this.selectedDeviceId,
+  //             firstChange: true,
+  //             isFirstChange: () => true,
+  //           },
+  //         };
+  //         this.historyTab.ngOnChanges(changes);
+  //       }
+  //       break;
+  //   }
+  // }
 
 }
--- src/app/admin/side-menu/practice/practice.component.ts
+++ src/app/admin/side-menu/practice/practice.component.ts
@@ -1,73 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component, ViewChild } from '@angular/core';
-import { HttpService } from '../../../service/http.service';
-import { AnalyticsComponent } from "../assets/analytics/analytics.component";
-import { HistoryComponent } from "../assets/history/history.component";
-import { ViewComponent } from "../assets/view/view.component";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-practice',
-  imports: [CommonModule,  ViewComponent, AnalyticsComponent, HistoryComponent],
+  imports: [CommonModule, ],
   templateUrl: './practice.component.html',
   styleUrl: './practice.component.css'
 })
 export class PracticeComponent {
 
-  isExpanded = false;
-  currentTab = 'viewTab';
-  data:any;
-  selectedDeviceId:any;
-  // @ViewChild('viewTab') viewTab!: ViewComponent;
-  // @ViewChild('analyticsTab') analyticsTab!: AnalyticsComponent;
-  // @ViewChild('historyTab') historyTab!: HistoryComponent;
-
-  constructor(private service:HttpService){};
-
-  ngOnInit(): void {
-    this.getAssetsList()
-  }
-  
-  getAssetsList() {
-    this.service.get('assets').subscribe((response: any[]) => {
-      this.data = response;
-    });
-  }
-
-  expandRow(devId:any): void {
-    this.isExpanded = !this.isExpanded;
-    this.selectedDeviceId = devId;
-  }
-
-  switchTab(tab: string): void {
-    this.currentTab = tab;
-    // this.invokeTabMethod(tab);
-  }
-
-  // invokeTabMethod(tab: string) {
-  //   switch (tab) {
-  //     case 'viewTab':
-  //       this.viewTab?.ngOnInit();
-  //       break;
-  //     case 'analyticsTab':
-  //       this.analyticsTab?.ngOnInit();
-  //       break;
-  //     // case 'historyTab':
-  //     //   this.historyTab?.ngOnChanges();
-  //     //   break;
-  //     case 'historyTab':
-  //       if (this.historyTab) {
-  //         const changes = {
-  //           deviceId: {
-  //             previousValue: null,
-  //             currentValue: this.selectedDeviceId,
-  //             firstChange: true,
-  //             isFirstChange: () => true,
-  //           },
-  //         };
-  //         this.historyTab.ngOnChanges(changes);
-  //       }
-  //       break;
-  //   }
-  // }
 
 }
